85ae23b30cba5e482c0d806ac1c37583a78e8628
Joe-metry <joemetry150@gmail.com>
2023-05-22 07:29:35 -0700

    Leveling up on Prompts and Alerts

---
+++ may_2023_JS_practice/levelingPromts.js
@@ -0,0 +1,9 @@
+/*Live coding exercise:
+
+In a single statement...
+
+Code an alert that displays...
+...the concatenation of a response to a prompt plus a string.*/
+
+var whatsNew = prompt("Tell us what's new?", "Software Engineering");
+alert(whatsNew + " " + "yea?");
