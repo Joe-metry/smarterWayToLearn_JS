a36954ff750cf80cc62e799fc1bf8e436b7fdfbf
Joe-metry <joemetry150@gmail.com>
2023-05-22 07:00:43 -0700

    alerts and more

---
+++ may_2023_JS_practice/alert2.js
@@ -0,0 +1,12 @@
+/*
+Live coding exercise:
+
+Assign a number value to a variable.
+Increment the variable using minimal code while assigning its incremented value to a second variable.
+Display the value of the second variable in an alert.
+*/
+
+
+var newNum = 91;
+var sec = ++newNum;
+alert(sec);
