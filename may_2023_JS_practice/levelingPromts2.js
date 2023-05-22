87a85c9b9443462c568d669208a1f08f6b2977d9
Joe-metry <joemetry150@gmail.com>
2023-05-22 07:34:24 -0700

    more exercises

---
+++ may_2023_JS_practice/levelingPromts2.js
@@ -0,0 +1,9 @@
+/*Assign strings to two variables. Code a prompt specifying the first
+ * variable as the message and the second variable as the default response.
+ * Assign the user's response to a third variable.
+ * None of the variables have been declared beforehand.
+ */
+
+var ui = "You";
+var ux = "Me";
+var uiux = prompt(ui, ux);
