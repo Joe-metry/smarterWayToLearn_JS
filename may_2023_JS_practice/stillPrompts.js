80e23a9da6713306253e375c1dfd9da325a7c9a2
Joe-metry <joemetry150@gmail.com>
2023-05-22 07:22:24 -0700

    Prompts and Alerts fun

---
+++ may_2023_JS_practice/stillPrompts.js
@@ -0,0 +1,8 @@
+/*ve coding exercise:
+
+Display a prompt, including both a message and a default response.
+Display the user's response in an alert.
+Click the Run It button to run your code live.*/
+
+var bestGuess = prompt("Guess What?", "JavaScript?");
+alert(bestGuess);
