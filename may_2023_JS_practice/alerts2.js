62101da494abb415c37f8d56b91251eac5b85b7b
Joe-metry <joemetry150@gmail.com>
2023-05-24 08:20:26 -0700

    Alerts Js

---
+++ may_2023_JS_practice/alerts2.js
@@ -0,0 +1,6 @@
+/*Code an if statement that tests whether a number equals itself.
+If the condition is true (it will be), display a congratulations alert.*/
+
+if (919 === 919) {
+ alert("Congrats!");
+}
