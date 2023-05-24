153c88025d6155cdde75d90a76be5c85578806d8
Joe-metry <joemetry150@gmail.com>
2023-05-24 08:46:30 -0700

    conditionals and comparison operators

---
+++ may_2023_JS_practice/conditionalA.js
@@ -0,0 +1,6 @@
+/*Code an if statement that tests whether a number is unequal to a different number.
+If the condition is true (it will be), display a congratulations alert.*/
+
+if (99989899 !== 8987988989987) {
+	alert("Congratulations buddy!");
+}
