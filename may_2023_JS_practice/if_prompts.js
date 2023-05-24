d76596b5b7b0b266a239524791339c163c9b6eca
Joe-metry <joemetry150@gmail.com>
2023-05-24 08:27:00 -0700

    conditionals with prompts and alerts.

---
+++ may_2023_JS_practice/if_prompts.js
@@ -0,0 +1,11 @@
+/*Code a prompt asking for your first name.
+Code an if statement that tests whether the name you entered equals your name.
+If the condition is true (it will be), display an alert that greets you by name.*/
+
+var firstName = "Joseph";
+var askFirstName = prompt("What is my first name?");
+
+if (firstName === askFirstName)
+{
+  alert("How are you Joseph?");
+}
