c176ccde3c51eb315b489a0ebbafa7a4fcbfa8a6
Joe-metry <joemetry150@gmail.com>
2023-05-24 08:52:11 -0700

    Conditionals, comparison operators, promps and alerts

---
+++ may_2023_JS_practice/conditionalB.js
@@ -0,0 +1,9 @@
+/*Code a prompt asking for your first name.
+Code an if statement that tests whether the name you entered is unequal to another name.
+If the condition is true (it will be), display an alert that says "No match"*/
+
+var firstName = prompt("Input any name:");
+if (firstName !== "Nelson")
+{
+  alert("No match");
+}
