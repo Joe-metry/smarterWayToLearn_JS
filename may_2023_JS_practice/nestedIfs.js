b7304c31ad552fbee11a83feeb4594f78a14d5ef
Joe-metry <joemetry150@gmail.com>
2023-05-25 10:01:46 -0700

    More control flow

---
+++ may_2023_JS_practice/nestedIfs.js
@@ -0,0 +1,8 @@
+*
+(a !== 0 && b > 1 && c < 1) {
+Code the first lines of nested if statements that test the same conditions as the above code.
+*/
+
+if (a !== 0) {
+  if (b > 1) {
+    if (c < 1) {
