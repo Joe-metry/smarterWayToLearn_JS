c4e79ff86ffadf1d8cc13640dfcbcbea00772e0c
Joe-metry <joemetry150@gmail.com>
2023-05-25 10:06:52 -0700

    More flow

---
+++ may_2023_JS_practice/prompCond.js
@@ -0,0 +1,13 @@
+/*
+Prompt the user to enter a city.
+If the city is Akron, display an alert acknowledging it.
+If not, display an alert acknowledging it isn't Akron.
+*/
+
+var city = prompt("enter a city");
+if (city === "Akron") {
+  alert("Warning");
+}
+else {
+  alert("safe");
+}
