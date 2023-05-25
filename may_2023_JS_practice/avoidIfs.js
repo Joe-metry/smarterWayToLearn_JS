fe6ff9bd0aa3552e6ae6860e3b8b3edfc2896baf
Joe-metry <joemetry150@gmail.com>
2023-05-25 10:03:36 -0700

    More on conditionals

---
+++ may_2023_JS_practice/avoidIfs.js
@@ -0,0 +1,12 @@
+/*
+if (a === 1) {
+  if (c === "Max") {
+    alert("OK");
+  }
+}
+Code an if statement that avoids the nesting above by testing for multiple conditions.
+*/
+
+if (a === 1 && c === "Max") {
+    alert("OK");
+}
