47144baf7e3ccc859e717d12c5a1b20da983ea3f
Joe-metry <joemetry150@gmail.com>
2023-05-29 10:25:55 -0700

    using the push meth on them

---
+++ may_2023_JS_practice/arrB.js
@@ -0,0 +1,7 @@
+/*Code an array with 1 string element.
+Add an additional element to the array using push.
+Create an alert whose message is the last element.*/
+
+var arrB = ["One"];
+arrB.push("Two");
+alert(arrB[1]);
