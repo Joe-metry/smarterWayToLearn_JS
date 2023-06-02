0d0478da10a9e62e490eee47124779c85b5408db
Joe-metry <joemetry150@gmail.com>
2023-06-02 08:06:42 -0700

    Using flags on Javascript

---
+++ June_2023_JS_practice/flags.js
@@ -0,0 +1,12 @@
+var givenCity = "Memphis";
+var flagg = false;
+var cleanCities = ["Honolulu", "Reading", "Ontario", "Memphis", "Colorado"];
+for (var j = 0; j < 5; i++) {
+    if (givenCity === cleanCities[j]) {
+        alert('The city given is one of the cleanest cities!')
+        flagg = True;
+    }
+if (flagg = false) {
+    alert('Oh, sorry the city is not in our list')
+} 
+}
