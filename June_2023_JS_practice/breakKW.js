988248f650daad96ce21e09d4794cd0c78caaacc
Joe-metry <joemetry150@gmail.com>
2023-06-06 08:54:34 -0700

    Looping and breaking

---
+++ June_2023_JS_practice/breakKW.js
@@ -0,0 +1,17 @@
+/*
+Create an array with 2 number elements.
+Set a for loop, limiting the number of iterations using the length keyword.
+Loop through the array to test whether each of the elements
+matches itself. (It will. )
+When a match is found, display an alert with the number (it should be your first number) and stop the loop.
+*/
+
+
+
+var numbers = [10, 11];
+for (var j = 0; j < numbers.length; j++) {
+  if (j === j ) {
+    alert(j);
+   break
+  }
+}
